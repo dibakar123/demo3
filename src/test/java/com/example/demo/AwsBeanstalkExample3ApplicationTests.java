577229ad19ab28c443d13f74b21eb2ea1750f396
dibakar123 <sanyaldibakar@gmail.com>
2020-07-09 12:27:20 +0530

    First upload to git

---
+++ src/test/java/com/example/demo/AwsBeanstalkExample3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsBeanstalkExample3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
